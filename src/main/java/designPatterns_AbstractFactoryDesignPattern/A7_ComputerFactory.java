c3e052c7fe3d49c701302e11cea2bc4c008e4883
kanomoku <1049505015@qq.com>
2021-08-25 16:14:42 +0800

    add

---
+++ src/main/java/designPatterns_AbstractFactoryDesignPattern/A7_ComputerFactory.java
@@ -0,0 +1,10 @@
+package designPatterns_AbstractFactoryDesignPattern;
+
+public class A7_ComputerFactory {
+
+	public static A1_Computer getComputer(A4_ComputerAbstractFactory factory){
+		return factory.createComputer();
+	}
+}
+
+
