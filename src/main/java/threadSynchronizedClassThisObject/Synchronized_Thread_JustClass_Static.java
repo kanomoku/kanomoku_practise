3896a888c7a66ba32941f36ef4ea027abc415b96
kanomoku <1049505015@qq.com>
2021-09-05 23:02:30 +0800

    add

---
+++ src/main/java/threadSynchronizedClassThisObject/Synchronized_Thread_JustClass_Static.java
@@ -0,0 +1,8 @@
+package threadSynchronizedClassThisObject;
+
+public class Synchronized_Thread_JustClass_Static extends Thread {
+    @Override
+    public void run() {
+    	Synchronized_ObjectService.serviceMethod3();
+    }
+}
---
+++ src/main/java/threadSynchronizedClassThisObject/Synchronized_Thread_JustClass_Sync_In_Static.java
@@ -0,0 +1,8 @@
+package threadSynchronizedClassThisObject;
+
+public class Synchronized_Thread_JustClass_Sync_In_Static extends Thread {
+    @Override
+    public void run() {
+    	Synchronized_ObjectService.serviceMethod_Sync_In_static();
+    }
+}
