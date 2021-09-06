19b395fda91824a628850822547e1df938d516b2
kanomoku <1049505015@qq.com>
2021-09-06 20:21:33 +0800

    jdk 动态代理

--- src/main/java/designPatterns12_ProxyPattern/C2_jdk_proxy_Boss.java
+++ src/main/java/designPatterns12_ProxyPattern/C2_jdk_proxy_Boss.java
@@ -3,20 +3,19 @@ package designPatterns12_ProxyPattern;
 public class C2_jdk_proxy_Boss implements C1_jdk_proxy_Function {
 	private String name;
 
+	public C2_jdk_proxy_Boss() {
+	}
+
 	public C2_jdk_proxy_Boss(String name) {
 		this.name = name;
 	}
 
-	public C2_jdk_proxy_Boss() {
-	}
-
 	public void target() {
-		System.out.println("制定一个小目标");
+		System.out.println(name +  "制定一个小目标");
 	}
 
 	@Override
 	public void eat(String str) {
-		System.out.println("老板吃" + str);
+		System.out.println(name + "老板吃" + str);
 	}
-
 }
--- src/main/java/designPatterns12_ProxyPattern/C4_jdk_proxy_Test.java
+++ src/main/java/designPatterns12_ProxyPattern/C4_jdk_proxy_Test.java
@@ -14,19 +14,20 @@ public class C4_jdk_proxy_Test {
 //			2.1 出现原因:希望把接口对象转换为具体真实对象
 //		
 
-		System.out.println(C1_jdk_proxy_Function.class.getClassLoader()==C2_jdk_proxy_Boss.class.getClassLoader());
-		
-		C3_jdk_proxy_Secretary mishu = new C3_jdk_proxy_Secretary();
-		//第一个参数：反射时使用的类加载器
-		//第二个参数：Proxy需要实现什么接口
-		//第三个参数：通过接口对象调用方法时，需要调用哪个类的invoke
-		C1_jdk_proxy_Function gongneng = (C1_jdk_proxy_Function) Proxy.newProxyInstance(
-				C1_jdk_proxy_Function.class.getClassLoader(),
-				new Class[] { C1_jdk_proxy_Function.class }, 
-				mishu);
-		
-		gongneng.eat("米饭");
-		gongneng.target();
-	}
+		System.out.println(C1_jdk_proxy_Function.class.getClassLoader() == C2_jdk_proxy_Boss.class.getClassLoader());
 
+//		C3_jdk_proxy_Secretary secretary = new C3_jdk_proxy_Secretary();
+
+		C2_jdk_proxy_Boss boss = new C2_jdk_proxy_Boss("云云");
+		C3_jdk_proxy_Secretary secretary = new C3_jdk_proxy_Secretary(boss);
+
+		// 第一个参数：反射时使用的类加载器
+		// 第二个参数：Proxy需要实现什么接口
+		// 第三个参数：通过接口对象调用方法时，需要调用哪个类的invoke
+		C1_jdk_proxy_Function function = (C1_jdk_proxy_Function) Proxy.newProxyInstance(
+				C1_jdk_proxy_Function.class.getClassLoader(), new Class[] { C1_jdk_proxy_Function.class }, secretary);
+
+		function.eat("米饭");
+		function.target();
+	}
 }
