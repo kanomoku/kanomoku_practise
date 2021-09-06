d3de3357f8760ae52e98d6e3baaf1593e0bff6a1
kanomoku <1049505015@qq.com>
2021-09-06 22:15:18 +0800

    适配器设计模式

---
+++ src/main/java/designPatterns6_AdapterDesignPattern/A3_SocketAdapter.java
@@ -0,0 +1,11 @@
+package designPatterns6_AdapterDesignPattern;
+
+public interface A3_SocketAdapter {
+
+	public A1_Volt get120Volt();
+		
+	public A1_Volt get12Volt();
+	
+	public A1_Volt get3Volt();
+}
+
