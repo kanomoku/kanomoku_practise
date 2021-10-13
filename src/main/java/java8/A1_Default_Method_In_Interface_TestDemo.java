651cd2ee1e66540733387212bceea92611418afc
kanomoku <1049505015@qq.com>
2021-10-13 20:22:48 +0800

    java8

--- src/main/java/java8/A1_Default_Method_In_Interface_TestDemo.java
+++ src/main/java/java8/A1_Default_Method_In_Interface_TestDemo.java
@@ -1,17 +1,17 @@
 package java8;
 
 public class A1_Default_Method_In_Interface_TestDemo {
-	public static void main(String[] args) {
-		A1_default_Method_In_Interface formula = new A1_default_Method_In_Interface() {
-			@Override
-			public double calculate(int a) {
-				return sqrt(a * 100);
-			}
-		};
+    public static void main(String[] args) {
+        A1_default_Method_In_Interface formula = new A1_default_Method_In_Interface() {
+            @Override
+            public double calculate(int a) {
+                return sqrt(a * 100);
+            }
+        };
 
-		System.out.println(formula.calculate(100)); // 100.0
-		System.out.println(formula.sqrt(16)); // 4.0
+        System.out.println(formula.calculate(100)); // 100.0
+        System.out.println(formula.sqrt(16)); // 4.0
 
-	}
+    }
 
 }
--- src/main/java/java8/A2_Converter_Functional_Interfaces.java
+++ src/main/java/java8/A2_Converter_Functional_Interfaces.java
@@ -2,5 +2,5 @@ package java8;
 
 @FunctionalInterface
 public interface A2_Converter_Functional_Interfaces<F, T> {
-  T convert(F from);
+    T convert(F from);
 }
