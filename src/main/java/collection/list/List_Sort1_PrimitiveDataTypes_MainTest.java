ee2e6a907fb94c25ca9a8e69e94a624f287fd73f
kanomoku <1049505015@qq.com>
2021-11-29 23:01:09 +0800

    list 排序的学习

--- src/main/java/collection/list/List_Sort1_PrimitiveDataTypes_MainTest.java
+++ src/main/java/collection/list/List_Sort1_PrimitiveDataTypes_MainTest.java
@@ -1,169 +1,169 @@
 package collection.list;
 
+import org.junit.Test;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-import org.junit.Test;
-
 public class List_Sort1_PrimitiveDataTypes_MainTest {
 
-	// -----------------------第1种----------------------------------------
-	@Test
-	public void test1() {
+    // -----------------------第1种----------------------------------------
+    @Test
+    public void test1() {
 
-		List<Integer> list = new ArrayList<>();
-		list.add(3);
-		list.add(5);
-		list.add(1);
-		list.add(10);
-		list.add(7);
-		list.add(7);
-		/**
-		 * Collections工具类，升序排:
-		 */
-		Collections.sort(list);
-		System.out.println(list);
-		System.out.println("Collections工具类，升序排:·················");
+        List<Integer> list = new ArrayList<>();
+        list.add(3);
+        list.add(5);
+        list.add(1);
+        list.add(10);
+        list.add(7);
+        list.add(7);
+        /**
+         * Collections工具类，升序排:
+         */
+        Collections.sort(list);
+        System.out.println(list);
+        System.out.println("Collections工具类，升序排:·················");
 
-	}
+    }
 
-	@Test
-	public void test2() {
-		List<Integer> list = new ArrayList<>();
-		list.add(3);
-		list.add(5);
-		list.add(1);
-		list.add(10);
-		list.add(7);
-		list.add(7);
-		/**
-		 * Collections工具类，降序排:
-		 */
-		Collections.sort(list);
-		Collections.reverse(list);
-		System.out.println(list);
-		System.out.println("Collections工具类，降序排:·················");
-	}
+    @Test
+    public void test2() {
+        List<Integer> list = new ArrayList<>();
+        list.add(3);
+        list.add(5);
+        list.add(1);
+        list.add(10);
+        list.add(7);
+        list.add(7);
+        /**
+         * Collections工具类，降序排:
+         */
+        Collections.sort(list);
+        Collections.reverse(list);
+        System.out.println(list);
+        System.out.println("Collections工具类，降序排:·················");
+    }
 
-	// -----------------------第2种----------------------------------------
-	@Test
-	public void test4() {
-		List<Integer> list = new ArrayList<>();
-		list.add(3);
-		list.add(5);
-		list.add(1);
-		list.add(10);
-		list.add(7);
-		list.add(7);
+    // -----------------------第2种----------------------------------------
+    @Test
+    public void test4() {
+        List<Integer> list = new ArrayList<>();
+        list.add(3);
+        list.add(5);
+        list.add(1);
+        list.add(10);
+        list.add(7);
+        list.add(7);
 
-		/**
-		 * 也可以使用自定义排序如下，效果一样，不过不够简洁
-		 */
-		list.sort(new Comparator<Integer>() {
-			@Override
-			public int compare(Integer o1, Integer o2) {
-				return o1 - o2;
-			}
-		});
-		System.out.println(list);
-		System.out.println("·················");
-	}
+        /**
+         * 也可以使用自定义排序如下，效果一样，不过不够简洁
+         */
+        list.sort(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o1 - o2;
+            }
+        });
+        System.out.println(list);
+        System.out.println("·················");
+    }
 
-	@Test
-	public void test5() {
-		List<Integer> list = new ArrayList<>();
-		list.add(3);
-		list.add(5);
-		list.add(1);
-		list.add(10);
-		list.add(7);
-		list.add(7);
-		/**
-		 * 自定义排序，降序排:
-		 */
-		list.sort(new Comparator<Integer>() {
-			@Override
-			public int compare(Integer o1, Integer o2) {
-				return o2 - o1;
-			}
-		});
-		System.out.println(list);
-		System.out.println("·················");
-	}
+    @Test
+    public void test5() {
+        List<Integer> list = new ArrayList<>();
+        list.add(3);
+        list.add(5);
+        list.add(1);
+        list.add(10);
+        list.add(7);
+        list.add(7);
+        /**
+         * 自定义排序，降序排:
+         */
+        list.sort(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2 - o1;
+            }
+        });
+        System.out.println(list);
+        System.out.println("·················");
+    }
 
-	// -----------------------第3种----------------------------------------
-	@Test
-	public void test6() {
-		List<Integer> list = new ArrayList<>();
-		list.add(3);
-		list.add(5);
-		list.add(1);
-		list.add(10);
-		list.add(7);
-		list.add(7);
-		/**
-		 * Lamda 表达式
-		 */
-		list.sort((a, b) -> a.compareTo(b));
-		System.out.println(list);
-		list.sort((a, b) -> b.compareTo(a));
-		System.out.println(list);
-		System.out.println("·················");
-	}
+    // -----------------------第3种----------------------------------------
+    @Test
+    public void test6() {
+        List<Integer> list = new ArrayList<>();
+        list.add(3);
+        list.add(5);
+        list.add(1);
+        list.add(10);
+        list.add(7);
+        list.add(7);
+        /**
+         * Lamda 表达式
+         */
+        list.sort((a, b) -> a.compareTo(b));
+        System.out.println(list);
+        list.sort((a, b) -> b.compareTo(a));
+        System.out.println(list);
+        System.out.println("·················");
+    }
 
-	@Test
-	public void test3() {
-		List<Integer> list = new ArrayList<>();
-		list.add(3);
-		list.add(5);
-		list.add(1);
-		list.add(10);
-		list.add(7);
-		list.add(7);
-		System.out.println(list);
+    @Test
+    public void test3() {
+        List<Integer> list = new ArrayList<>();
+        list.add(3);
+        list.add(5);
+        list.add(1);
+        list.add(10);
+        list.add(7);
+        list.add(7);
+        System.out.println(list);
 
-		// -----------------------第4种----------------------------------------
-		list.sort(Integer::compareTo);
-		System.out.println(list);
+        // -----------------------第4种----------------------------------------
+        list.sort(Integer::compareTo);
+        System.out.println(list);
 
-		list.sort(Comparator.naturalOrder());
-		System.out.println(list);
-		list.sort(Comparator.reverseOrder());
-		System.out.println(list);
+        list.sort(Comparator.naturalOrder());
+        System.out.println(list);
+        list.sort(Comparator.reverseOrder());
+        System.out.println(list);
 
-		// -----------------------第5种----------------------------------------
+        // -----------------------第5种----------------------------------------
 
-		list.stream().sorted((a, b) -> a.compareTo(b)).forEach(System.out::print);// 升序
-		System.out.println();
+        list.stream().sorted((a, b) -> a.compareTo(b)).forEach(System.out::print);// 升序
+        System.out.println();
 
-		list.stream().sorted((a, b) -> b.compareTo(a)).forEach(System.out::print);// 降序
-		System.out.println();
-		// -----------------------第6种----------------------------------------
-		list.stream().sorted(Comparator.naturalOrder()).forEach(System.out::print);// 升序
-		System.out.println();
+        list.stream().sorted((a, b) -> b.compareTo(a)).forEach(System.out::print);// 降序
+        System.out.println();
+        // -----------------------第6种----------------------------------------
+        list.stream().sorted(Comparator.naturalOrder()).forEach(System.out::print);// 升序
+        System.out.println();
 
-		list.stream().sorted(Comparator.reverseOrder()).forEach(System.out::print);// 降序
-		System.out.println();
-	}
-	
-//  Collections.sort(list); 
-//	list.sort(Comparator<? super E> c) 
-//	list.stream().sorted(Comparator<? super T> comparator);
-	
-//	new Comparator<Integer>() {
-//		@Override
-//		public int compare(Integer o1, Integer o2) {
-//			return o2 - o1;
-//		}
-//	}
-	
-//  以下三种需要 元素本身继承Comparator<? super T> 并且Override compareTo方法
-//	(a, b) -> a.compareTo(b)
-	
-//	Integer::compareTo
-	
-//	Comparator.naturalOrder()
-//	Comparator.reverseOrder()
+        list.stream().sorted(Comparator.reverseOrder()).forEach(System.out::print);// 降序
+        System.out.println();
+    }
+
+    // Collections.sort(list);
+    // list.sort(Comparator<? super E> c)
+    // list.stream().sorted(Comparator<? super T> comparator);
+
+    // new Comparator<Integer>() {
+    // @Override
+    // public int compare(Integer o1, Integer o2) {
+    // return o2 - o1;
+    // }
+    // }
+
+    // 以下三种需要 元素本身继承Comparator<? super T> 并且Override compareTo方法
+    // (a, b) -> a.compareTo(b)
+
+    // Integer::compareTo
+
+    // Comparator.naturalOrder()
+    // Comparator.reverseOrder()
 }
--- src/main/java/collection/list/List_Sort3_Student_No_Implements_Comparable_MainTest.java
+++ src/main/java/collection/list/List_Sort3_Student_No_Implements_Comparable_MainTest.java
@@ -1,6 +1,9 @@
 package collection.list;
 
+import lombok.Data;
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
@@ -18,66 +21,87 @@ public class List_Sort3_Student_No_Implements_Comparable_MainTest {
 		System.out.println("排序之前的list：");
 		studentList.stream().forEach(System.out::println);
 
-//		// 自定义排序1
-//		Collections.sort(studentList, (o1, o2) -> {
-//			// 先按照年龄排序
-//			Integer i = o1.getAge().compareTo(o2.getAge());
-//			if (i == 0) {
-//				// 如果年龄相等，则按照分数排序
-//				i = o1.getScore().compareTo(o2.getScore());
-//				if (i == 0) {
-//					// 如果分数相等，则按照姓名排序
-//					i = o1.getName().compareTo(o2.getName());
-//				}
-//			}
-//			return i;
-//		});
-//		System.out.println("排序之后的list：");
-//		studentList.stream().forEach(System.out::println);
+		// 自定义排序1
+		Collections.sort(studentList, (o1, o2) -> {
+			// 先按照年龄排序
+			Integer i = o1.getAge().compareTo(o2.getAge());
+			if (i == 0) {
+				// 如果年龄相等，则按照分数排序
+				i = o1.getScore().compareTo(o2.getScore());
+				if (i == 0) {
+					// 如果分数相等，则按照姓名排序
+					i = o1.getName().compareTo(o2.getName());
+				}
+			}
+			return i;
+		});
+		System.out.println("排序之后的list：");
+		studentList.stream().forEach(System.out::println);
 		
-//		// 自定义排序2
-//		studentList.sort((o1, o2) -> {
-//			// 先按照年龄排序
-//			Integer i = o1.getAge().compareTo(o2.getAge());
-//			if (i == 0) {
-//				// 如果年龄相等，则按照分数排序
-//				i = o1.getScore().compareTo(o2.getScore());
-//				if (i == 0) {
-//					// 如果分数相等，则按照姓名排序
-//					i = o1.getName().compareTo(o2.getName());
-//				}
-//			}
-//			return i;
-//		});
-//		System.out.println("排序之后的list：");
-//		studentList.stream().forEach(System.out::println);
+		// 自定义排序2
+		studentList.sort((o1, o2) -> {
+			// 先按照年龄排序
+			Integer i = o1.getAge().compareTo(o2.getAge());
+			if (i == 0) {
+				// 如果年龄相等，则按照分数排序
+				i = o1.getScore().compareTo(o2.getScore());
+				if (i == 0) {
+					// 如果分数相等，则按照姓名排序
+					i = o1.getName().compareTo(o2.getName());
+				}
+			}
+			return i;
+		});
+		System.out.println("排序之后的list：");
+		studentList.stream().forEach(System.out::println);
 
-//		// 只按照姓名排序方式一：
-//		Collections.sort(studentList, (e1, e2) -> e1.getName().compareTo(e2.getName()));
-//		// 只按照姓名排序方式二：
-//		Collections.sort(studentList, Comparator.comparing(Student_No_Implements_Comparable::getName));
-//		// 只按照姓名排序方式三：
-//		Collections.sort(studentList, Comparator.comparing(e -> e.getName()));
-//		
-//		// 只按照姓名排序方式四：
-//		studentList.sort((e1, e2) -> e1.getName().compareTo(e2.getName()));
-//		// 只按照姓名排序方式五：
-//		studentList.sort(Comparator.comparing(Student_No_Implements_Comparable::getName));
-//		// 只按照姓名排序方式六：
-//		studentList.sort(Comparator.comparing(e -> e.getName()));
-//
-//		// 只按照年龄排序方式一：
-//		Collections.sort(studentList, (e1, e2) -> e1.getAge().compareTo(e2.getAge()));
-//		// 只按照年龄排序方式二：
-//		Collections.sort(studentList, Comparator.comparingInt(Student_No_Implements_Comparable::getAge));
-//		System.out.println("只按照年龄排序之后的list：");
-//		studentList.stream().forEach(System.out::println);
+		System.out.println("排序之后的2222");
+		studentList.sort(new Comparator<Student_No_Implements_Comparable>() {
+			@Override
+			public int compare(Student_No_Implements_Comparable o1, Student_No_Implements_Comparable o2) {
+				// 先按照年龄排序
+				Integer i = o1.getAge().compareTo(o2.getAge());
+				if (i == 0) {
+					// 如果年龄相等，则按照分数排序
+					i = o1.getScore().compareTo(o2.getScore());
+					if (i == 0) {
+						// 如果分数相等，则按照姓名排序
+						i = o1.getName().compareTo(o2.getName());
+					}
+				}
+				return i;
+			}
+		});
+		studentList.stream().forEach(System.out::println);
+		System.out.println("排序之后的222");
+
+		// 只按照姓名排序方式一：
+		Collections.sort(studentList, (e1, e2) -> e1.getName().compareTo(e2.getName()));
+		// 只按照姓名排序方式二：
+		Collections.sort(studentList, Comparator.comparing(Student_No_Implements_Comparable::getName));
+		// 只按照姓名排序方式三：
+		Collections.sort(studentList, Comparator.comparing(e -> e.getName()));
+
+		// 只按照姓名排序方式四：
+		studentList.sort((e1, e2) -> e1.getName().compareTo(e2.getName()));
+		// 只按照姓名排序方式五：
+		studentList.sort(Comparator.comparing(Student_No_Implements_Comparable::getName));
+		// 只按照姓名排序方式六：
+		studentList.sort(Comparator.comparing(e -> e.getName()));
+
+		// 只按照年龄排序方式一：
+		Collections.sort(studentList, (e1, e2) -> e1.getAge().compareTo(e2.getAge()));
+		// 只按照年龄排序方式二：
+		Collections.sort(studentList, Comparator.comparingInt(Student_No_Implements_Comparable::getAge));
+		System.out.println("只按照年龄排序之后的list：");
+		studentList.stream().forEach(System.out::println);
 		
 		studentList.stream().sorted(Comparator.comparingInt(Student_No_Implements_Comparable::getAge)).forEach(System.out::println);;
 
 	}
 }
 
+@Data
 class Student_No_Implements_Comparable {
 	private Integer age;
 	private Integer score;
